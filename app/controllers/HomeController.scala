ff7b6fbe956d9cef1991f98f5473b65bce184fa3
Pete1232 <pete1232@gmail.com>
2016-07-16 11:10:53 +0100

    Refactor of home controller and tidy up

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,28 +1,15 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 /**
-  * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
+  * Controller for the landing page
   */
-@Singleton
-class HomeController @Inject() (webJarAssets: WebJarAssets) extends Controller {
-
-  /**
-    * Create an Action to render an HTML page with a welcome message.
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready.")(webJarAssets))
-  }
+class HomeController @Inject()(implicit webJarAssets: WebJarAssets) extends Controller {
 
   def home = Action {
-    Ok(views.html.home(webJarAssets))
+    Ok(views.html.home())
   }
 
 }
