33c0b32ce1b453f13d971a5405f13fcd287efc80
Pete1232 <pete1232@gmail.com>
2016-07-22 11:50:53 +0100

    Finished simple account page

--- app/controllers/AccountController.scala
+++ app/controllers/AccountController.scala
@@ -4,7 +4,10 @@ import com.google.inject.Inject
 import play.api.mvc.{Action, Controller}
 
 class AccountController @Inject()(implicit webJarAssets: WebJarAssets) extends Controller{
-  def displayUserDetails = Action {
-    Ok(views.html.account())
+  def displayUserDetails = Action { implicit request =>
+    request2session.get("userId") match {
+      case Some(userId) => Ok(views.html.account(userId))
+      case _ => Redirect(routes.LoginController.login())
+    }
   }
 }
--- test/controllers/AccountControllerSpec.scala
+++ test/controllers/AccountControllerSpec.scala
@@ -1,13 +1,23 @@
 package controllers
 
 import config.ControllerSpec
+import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
 class AccountControllerSpec extends ControllerSpec{
   val controller = new AccountController()
-  "displayUserDetails" must "load the user details page" in {
-    val result = controller.displayUserDetails.apply(simpleRequest)
+  "displayUserDetails" must "allow an authenticated user to view the page" in running(application){
+    val result = controller.displayUserDetails
+      .apply(
+        FakeRequest()
+          .withSession("userId" -> "user")
+      )
     status(result) mustBe 200
     contentAsString(result) must include("<title>Your Account</title>")
+  }
+  it should "redirect an unauthenticated user to login" in {
+    val result = controller.displayUserDetails.apply(simpleRequest)
+    status(result) mustBe 303
+    redirectLocation(result).get mustBe ("/login")
   }
 }
