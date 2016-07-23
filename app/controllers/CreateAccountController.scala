5130fc61901c9f85c73c03c17c9f16644924ac7f
Pete1232 <pete1232@gmail.com>
2016-07-23 20:16:27 +0100

    Added more to create account impl

--- app/controllers/CreateAccountController.scala
+++ app/controllers/CreateAccountController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import com.google.inject.Inject
 import forms.CreateAccountForm
+import play.Logger
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
 
@@ -10,5 +11,18 @@ class CreateAccountController @Inject()(implicit webJarAssets: WebJarAssets, val
   val accountForm = new CreateAccountForm().form
   val create = Action {
     Ok(views.html.create(accountForm))
+  }
+
+  val createPost = Action { implicit request =>
+    accountForm.bindFromRequest.fold(
+      formWithErrors => {
+        formWithErrors.errors.foreach(error => Logger.info(s"Validation error on field ${error.messages}"))
+        BadRequest(views.html.create(formWithErrors))
+      },
+      userData => {
+        Redirect(routes.HomeController.home)
+          .withSession("userId" -> userData.userId)
+      }
+    )
   }
 }
--- test/controllers/CreateAccountControllerSpec.scala
+++ test/controllers/CreateAccountControllerSpec.scala
@@ -1,10 +1,12 @@
 package controllers
 
 import config.ControllerSpec
-import play.api.i18n.MessagesApi
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.mvc.Session
+import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
-class CreateAccountControllerSpec extends ControllerSpec{
+class CreateAccountControllerSpec extends ControllerSpec with I18nSupport{
   implicit val messagesApi = application.injector.instanceOf[MessagesApi]
   val controller = new CreateAccountController
   val result = controller.create.apply(simpleRequest)
@@ -19,5 +21,22 @@ class CreateAccountControllerSpec extends ControllerSpec{
     resultString must include regex("""(<form)(.*)(id="createAccountForm">)""".r)
     resultString must include("<input type=\"text\" id=\"userId\" name=\"userId\"")
     resultString must include("<input type=\"text\" id=\"password\" name=\"password\"")
+  }
+  "createPost" must "return a form with errors if the form had missing data" in {
+    val fieldRequiredResult = controller.createPost.apply(simpleRequest)
+    val resultString = contentAsString(fieldRequiredResult)
+    resultString must include("<div class=\"alert-message error\">")
+    resultString must include(Messages("error.required", "userId"))
+    resultString must include(Messages("error.required", "password"))
+  }
+  it must "redirect to home with a session cookie after post" in running(application) {
+    val createSuccessResult = controller.createPost
+      .apply(FakeRequest.apply()
+        .withFormUrlEncodedBody(
+          "userId" -> "user",
+          "password" -> "password"
+        ))
+    status(createSuccessResult) mustBe 303
+    session(createSuccessResult) mustBe (Session(Map("userId" -> "user")))
   }
 }
