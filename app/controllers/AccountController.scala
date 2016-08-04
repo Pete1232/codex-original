083b467c5aa450f905334afa448fd4f1734ea717
Pete1232 <pete1232@gmail.com>
2016-08-04 23:12:20 +0100

    Added delete method to Account controller

--- app/controllers/AccountController.scala
+++ app/controllers/AccountController.scala
@@ -1,13 +1,37 @@
 package controllers
 
 import com.google.inject.Inject
+import connectors.UserDatabaseConnector
+import models.User
+import play.Logger
 import play.api.mvc.{Action, Controller}
 
-class AccountController @Inject()(implicit webJarAssets: WebJarAssets) extends Controller{
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class AccountController @Inject()(userDatabaseConnector: UserDatabaseConnector)(implicit webJarAssets: WebJarAssets) extends Controller{
   def displayUserDetails = Action { implicit request =>
     request2session.get("userId") match {
       case Some(userId) => Ok(views.html.account(userId))
       case _ => Redirect(routes.LoginController.login(Some("/account")))
+    }
+  }
+
+  def deleteUser = Action.async { implicit request =>
+    request2session.get("userId") match {
+      case Some(userId) => {
+        userDatabaseConnector.deleteUser(User(userId, ""))
+          .map { success =>
+            Redirect(routes.LogoutController.logout)
+          }
+          .recover{
+            case e: Exception => {
+              Logger.error(s"Fatal exception $e during user delete")
+              Redirect(routes.ErrorController.error)
+            }
+          }
+      }
+      case _ => Future.successful(Redirect(routes.LoginController.login(Some("/account"))))
     }
   }
 }
--- test/controllers/AccountControllerSpec.scala
+++ test/controllers/AccountControllerSpec.scala
@@ -1,11 +1,13 @@
 package controllers
 
 import config.ControllerSpec
+import connectors.UserDatabaseConnector
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
 class AccountControllerSpec extends ControllerSpec{
-  val controller = new AccountController()
+  implicit val userDatabaseConnector = application.injector.instanceOf[UserDatabaseConnector]
+  val controller = new AccountController(userDatabaseConnector)
   "displayUserDetails" must "allow an authenticated user to view the page" in running(application){
     val result = controller.displayUserDetails
       .apply(
@@ -19,5 +21,15 @@ class AccountControllerSpec extends ControllerSpec{
     val result = controller.displayUserDetails.apply(simpleRequest)
     status(result) mustBe 303
     redirectLocation(result).get mustBe ("/login?continueUrl=%2Faccount")
+  }
+  "deleteUser" must "redirect to the logout controller" in running(application){
+    val result = controller.deleteUser.apply(simpleRequest.withSession("userId" -> "user"))
+    status(result) mustBe 303
+    redirectLocation(result).get mustBe "/logout"
+  }
+  it must "redirect to the login page if the session isn't found" in {
+    val result = controller.deleteUser.apply(simpleRequest)
+    status(result) mustBe 303
+    redirectLocation(result).get mustBe "/login?continueUrl=%2Faccount"
   }
 }
